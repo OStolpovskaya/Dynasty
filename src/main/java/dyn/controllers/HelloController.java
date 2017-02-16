46910d4231fc88ef3486c265b218888b2949f92c
OStolpovskaya <o.stolpovskaya@gmail.com>
2017-02-17 01:48:34 +0300

    spring boot application, hello controller and test

---
+++ src/main/java/dyn/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package dyn.controllers;
+
+/**
+ * Created by OM on 17.02.2017.
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
